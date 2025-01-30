ccd44877edd3a612f257a6de8cabac4e7338e55c
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-01-30 09:02:41 +0100

    reset auto-build to the saved value

--- org.eclipse.xtext.xtext.ui.tests/src/org/eclipse/xtext/xtext/ui/editor/quickfix/InternalBuilderTest.java
+++ org.eclipse.xtext.xtext.ui.tests/src/org/eclipse/xtext/xtext/ui/editor/quickfix/InternalBuilderTest.java
@@ -31,12 +31,27 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.internal.core.JavaModelManager;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.ui.texteditor.MarkerUtilities;
+import org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * @author dhuebner - Initial contribution and API
  */
 public class InternalBuilderTest {
+
+	private boolean wasAutoBuild;
+
+	@Before
+	public void saveAutoBuild() {
+		wasAutoBuild = IResourcesSetupUtil.isAutobuild();
+	}
+
+	@After
+	public void resetAutoBuild() {
+		setAutoBuild(wasAutoBuild);
+	}
 
 	@Test
 	public void test() throws CoreException, FileNotFoundException {
