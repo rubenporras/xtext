db4825a2b8286c99aa20d84fff98b3a87c0e1359
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2025-04-05 09:24:16 +0200

    MWE 2.22.0.M0
    Signed-off-by: Christian Dietrich <christian.dietrich.opensource@gmail.com>

--- org.eclipse.xtext.util/src/org/eclipse/xtext/util/XtextVersion.java
+++ org.eclipse.xtext.util/src/org/eclipse/xtext/util/XtextVersion.java
@@ -52,14 +52,14 @@ public class XtextVersion {
 	 * @noreference
 	 */
 	public String getMweVersion() {
-		return "2.21.0";
+		return "2.22.0.M0";
 	}
 
 	/**
 	 * @noreference
 	 */
 	public String getMweBuildNumber() {
-		return "";
+		return "S202504050710";
 	}
 
 	public String getAntlrGeneratorVersion() {
