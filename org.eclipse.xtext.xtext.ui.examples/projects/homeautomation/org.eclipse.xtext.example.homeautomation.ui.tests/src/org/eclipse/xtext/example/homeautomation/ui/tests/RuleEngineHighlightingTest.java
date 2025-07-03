49e147bbbc2ec5a80151e7c9a7a0d71a470c3ab4
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2025-07-03 10:23:47 +0200

    fix sample tests

--- org.eclipse.xtext.xtext.ui.examples/projects/homeautomation/org.eclipse.xtext.example.homeautomation.ui.tests/src/org/eclipse/xtext/example/homeautomation/ui/tests/RuleEngineHighlightingTest.java
+++ org.eclipse.xtext.xtext.ui.examples/projects/homeautomation/org.eclipse.xtext.example.homeautomation.ui.tests/src/org/eclipse/xtext/example/homeautomation/ui/tests/RuleEngineHighlightingTest.java
@@ -44,7 +44,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
 	@Test
 	public void device_keyword() {
 		testHighlighting("""
-			Device Window can be open, closed
+		Device Window can be open, closed
 		""", "Device", xbaseHighlightingConfiguration.keywordTextStyle());
 	}
 
@@ -58,219 +58,219 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
 	@Test
 	public void be_keyword() {
 		testHighlighting("""
-			Device Window can be open, closed
+		Device Window can be open, closed
 		""", "be", xbaseHighlightingConfiguration.keywordTextStyle());
 	}
 
 	@Test
 	public void rule_keyword() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule 'rule1' when Window.open then
-				fire(Heater.off)
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule 'rule1' when Window.open then
+			fire(Heater.off)
 		""", "Rule", xbaseHighlightingConfiguration.keywordTextStyle());
 	}
 
 	@Test
 	public void when_keyword() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule 'rule1' when Window.open then
-				fire(Heater.off)
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule 'rule1' when Window.open then
+			fire(Heater.off)
 		""", "when", xbaseHighlightingConfiguration.keywordTextStyle());
 	}
 
 	@Test
 	public void then_keyword() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule 'rule1' when Window.open then
-				fire(Heater.off)
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule 'rule1' when Window.open then
+			fire(Heater.off)
 		""", "then", xbaseHighlightingConfiguration.keywordTextStyle());
 	}
 
 	@Test
 	public void switch_keyword() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off
-			
-			Rule "Save energy" when Window.open then
-				switch new java.util.Random().nextInt
-					case 1,
-					case 2:
-						fire(Heater.off)
-					default:
-						fire(Heater.on)
+		Device Window can be open, closed
+		Device Heater can be on, off
+		
+		Rule "Save energy" when Window.open then
+			switch new java.util.Random().nextInt
+				case 1,
+				case 2:
+					fire(Heater.off)
+				default:
+					fire(Heater.on)
 		""", "switch", xbaseHighlightingConfiguration.keywordTextStyle());
 	}
 
 	@Test
 	public void case_keyword() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off
-			
-			Rule "Save energy" when Window.open then
-				switch new java.util.Random().nextInt
-					case 1,
-					case 2:
-						fire(Heater.off)
-					default:
-						fire(Heater.on)
+		Device Window can be open, closed
+		Device Heater can be on, off
+		
+		Rule "Save energy" when Window.open then
+			switch new java.util.Random().nextInt
+				case 1,
+				case 2:
+					fire(Heater.off)
+				default:
+					fire(Heater.on)
 		""", "case", xbaseHighlightingConfiguration.keywordTextStyle());
 	}
 
 	@Test
 	public void default_keyword() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off
-			
-			Rule "Save energy" when Window.open then
-				switch new java.util.Random().nextInt
-					case 1,
-					case 2:
-						fire(Heater.off)
-					default:
-						fire(Heater.on)
+		Device Window can be open, closed
+		Device Heater can be on, off
+		
+		Rule "Save energy" when Window.open then
+			switch new java.util.Random().nextInt
+				case 1,
+				case 2:
+					fire(Heater.off)
+				default:
+					fire(Heater.on)
 		""", "default", xbaseHighlightingConfiguration.keywordTextStyle());
 	}
 
 	@Test
 	public void number() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off
-			
-			Rule "Save energy" when Window.open then
-				switch new java.util.Random().nextInt
-					case 1,
-					case 2:
-						fire(Heater.off)
-					default:
-						fire(Heater.on)
+		Device Window can be open, closed
+		Device Heater can be on, off
+		
+		Rule "Save energy" when Window.open then
+			switch new java.util.Random().nextInt
+				case 1,
+				case 2:
+					fire(Heater.off)
+				default:
+					fire(Heater.on)
 		""", "1", xbaseHighlightingConfiguration.numberTextStyle());
 	}
 
 	@Test
 	public void punctuation() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule 'rule1' when Window.open then
-				fire(Heater.off)
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule 'rule1' when Window.open then
+			fire(Heater.off)
 		""", ".", xbaseHighlightingConfiguration.punctuationTextStyle());
 	}
 
 	@Test
 	public void single_quoted_rule_description() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule 'rule1' when Window.open then
-				fire(Heater.off)
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule 'rule1' when Window.open then
+			fire(Heater.off)
 		""", "rule1", xbaseHighlightingConfiguration.stringTextStyle());
 	}
 
 	@Test
 	public void double_quoted_rule_description() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule "rule1" when Window.open then
-				fire(Heater.off)
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule "rule1" when Window.open then
+			fire(Heater.off)
 		""", "rule1", xbaseHighlightingConfiguration.stringTextStyle());
 	}
 
 	@Test
 	public void fire_method_invocation() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule "rule1" when Window.open then
-				fire(Heater.off)
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule "rule1" when Window.open then
+			fire(Heater.off)
 		""", "fire", xbaseHighlightingConfiguration.staticMethodInvocation());
 	}
 
 	@Test
 	public void device_state_access() {
 		testHighlighting("""
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule "rule1" when Window.open then
-				fire(Heater.off)
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule "rule1" when Window.open then
+			fire(Heater.off)
 		""", "off", xbaseHighlightingConfiguration.staticField());
 	}
 
 	@Test
 	public void single_line_comment() {
 		testHighlighting("""
-			// A language for home automation systems.
-			
-			Device Window can be open, closed
-			Device Heater can be on, off, error
-			
-			Rule "rule1" when Window.open then
-				fire(Heater.off)
+		// A language for home automation systems.
+		
+		Device Window can be open, closed
+		Device Heater can be on, off, error
+		
+		Rule "rule1" when Window.open then
+			fire(Heater.off)
 		""", "A language for home automation systems", xbaseHighlightingConfiguration.commentTextStyle());
 	}
 
 	@Test
 	public void multi_line_comment() {
 		testHighlighting("""
-			/*
-			 * A language for home automation systems
-			 * supporting indentation-based code blocks
-			 * similar to Python.
-			 */
+		/*
+		 * A language for home automation systems
+		 * supporting indentation-based code blocks
+		 * similar to Python.
+		 */
 		""", """
-			/*
-			 * A language for home automation systems
-			 * supporting indentation-based code blocks
-			 * similar to Python.
-			 */
+		/*
+		 * A language for home automation systems
+		 * supporting indentation-based code blocks
+		 * similar to Python.
+		 */
 		""", xbaseHighlightingConfiguration.commentTextStyle());
 	}
 
 	@Test
 	public void fixme_task_in_comment() {
 		testHighlighting("""
-			/**
-			 * FIXME
-			 */
-			Device Window can be open, closed
+		/**
+		 * FIXME
+		 */
+		Device Window can be open, closed
 		""", "FIXME", xbaseHighlightingConfiguration.taskTextStyle());
 	}
 
 	@Test
 	public void todo_task_in_comment() {
 		testHighlighting("""
-			/**
-			 * TODO
-			 */
-			Device Window can be open, closed
+		/**
+		 * TODO
+		 */
+		Device Window can be open, closed
 		""", "TODO", xbaseHighlightingConfiguration.taskTextStyle());
 	}
 
 	@Test
 	public void xxx_task_in_comment() {
 		testHighlighting("""
-			/**
-			 * XXX
-			 */
-			Device Window can be open, closed
+		/**
+		 * XXX
+		 */
+		Device Window can be open, closed
 		""", "XXX", xbaseHighlightingConfiguration.taskTextStyle());
 	}
 
